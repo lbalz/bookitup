c5fe6b8420338639669274b43e779cc967d5d849
Lukas Balz <lukasbalz.business@gmail.com>
2025-07-05 20:39:32 +0200

    added first i18n setup

---
+++ frontend/bookitup-frontend/src/components/Header.vue
@@ -0,0 +1,33 @@
+<script setup>
+    import '../assets/styles/_variables.css';
+    import { useI18n } from 'vue-i18n';
+    const { locale } = useI18n();
+
+    const currentLocale = locale.value;
+
+    function changeLocale(event) {
+        locale.value = event.target.value;
+    }
+</script>
+
+<template>
+    <header>
+        <h3>Bookitup</h3>
+
+        <nav>
+            <a href="">{{ $t('header.aboutUs') }}</a>
+        </nav>
+    </header>
+
+    <!--TODO: Von select in DE | EN ändern -->
+    <!--TODO: Ein paar farben und schriftart raussuchen -->
+    <select v-model="currentLocale" @change="changeLocale">
+        <option value="de">Deutsch</option>
+        <option value="en">English</option>
+    </select>
+</template>
+
+<style scoped>
+    
+
+</style>
--- frontend/bookitup-frontend/src/main.js
+++ frontend/bookitup-frontend/src/main.js
@@ -1,5 +1,22 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import { createI18n } from 'vue-i18n'
 
-createApp(App).mount('#app')
+import de from './locales/de.json'
+import en from './locales/en.json'
+
+const messages = {
+    de,
+    en
+};
+
+const i18n = createI18n({
+    locale: 'de', // Set default locale
+    fallbackLocale: 'en', // Fallback locale
+    messages,
+});
+
+const app = createApp(App);
+app.use(i18n);
+app.mount('#app');
